8ea1ad3ff6bdfcaeca4979801588f2f2ec247a58
Flavia0724 <161131964+FlaviaEtec24@users.noreply.github.com>
2024-03-12 20:18:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Trem bala");
+Console.WriteLine("Não é sobre ter todas as pessoas do mundo pra si");
+Console.WriteLine("É sobre saber que em algum lugar alguém zela por ti");
+Console.WriteLine("É sobre cantar e poder escutar mais do que a própria voz");
+Console.WriteLine("É sobre dançar na chuva de vida que cai sobre nós");
+
+Console.WriteLine("É saber se sentir infinito");
+Console.WriteLine("Num universo tão vasto e bonito, é saber sonhar");
+Console.WriteLine("Então fazer valer a pena");
+Console.WriteLine("Cada verso daquele poema sobre acreditar");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Não é sobre chegar no topo do mundo e saber que venceu");
+Console.WriteLine("É sobre escalar e sentir que o caminho te fortaleceu");
+Console.WriteLine("É sobre ser abrigo e também ter morada em outros corações");
+Console.WriteLine("E assim ter amigos contigo em todas as situações");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("A gente não pode ter tudo");
+Console.WriteLine("Qual seria a graça do mundo se fosse assim?");
+Console.WriteLine("Por isso, eu prefiro sorrisos");
+Console.WriteLine("E os presentes que a vida trouxe pra perto de mim");
+
+Console.WriteLine("Não é sobre tudo que o seu dinheiro é capaz de comprar");
+Console.WriteLine("E sim sobre cada momento, sorriso a se compartilhar");
+Console.WriteLine("Também não é sobre correr contra o tempo pra ter sempre mais");
+Console.WriteLine("Porque quando menos se espera a vida já ficou pra trás");
+
+Console.WriteLine("Segura teu filho no colo");
+Console.WriteLine("Sorria e abraça os teus pais enquanto estão aqui");
+Console.WriteLine("Que a vida é trem-bala, parceiro");
+Console.WriteLine("E a gente é só passageiro prestes a partir");
+
+Console.WriteLine("Laiá, laiá, laiá, laiá, laiá");
+Console.WriteLine("Laiá, laiá, laiá, laiá, laiá");
+
+Console.WriteLine("Segura teu filho no colo");
+Console.WriteLine("Sorria e abraça os teus pais enquanto estão aqui");
+Console.WriteLine("Que a vida é trem-bala, parceiro");
+Console.WriteLine("E a gente é só passageiro prestes a partir");
